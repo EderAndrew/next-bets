788c1844252b672efd8ae796b94a6326290fb632
Eder Andrew <ederandrew0028@gmail.com>
2024-01-23 14:44:02 -0300

    Create new modules packages

--- src/context/Web3Modal.tsx
+++ src/context/Web3Modal.tsx
@@ -1,10 +1,20 @@
 'use client'
-import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi1/react'
-import { WagmiConfig } from 'wagmi'
-import { arbitrum, mainnet } from 'viem/chains'
+import { createWeb3Modal } from '@web3modal/wagmi1/react'
+import { walletConnectProvider, EIP6963Connector } from '@web3modal/wagmi1'
+import { WagmiConfig, configureChains, createConfig } from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
+import { mainnet } from 'viem/chains'
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 import { ReactNode } from 'react'
 
 const projectId = 'YOUR_PROJECT_ID'
+
+const { chains, publicClient } = configureChains(
+    [mainnet],
+    [walletConnectProvider({ projectId }), publicProvider()]
+  )
 
 const metadata = {
     name:'Web3Modal',
@@ -13,8 +23,16 @@ const metadata = {
     icons: ['https://avatars.githubusercontent.com/u/37784886']
 }
 
-const chains = [arbitrum, mainnet]
-const wagmiConfig: any = defaultWagmiConfig({ projectId, chains, metadata })
+const wagmiConfig: any = createConfig({ 
+    autoConnect: true,
+    connectors: [
+        new WalletConnectConnector({ chains, options: { projectId, showQrModal: false, metadata } }),
+        new EIP6963Connector({ chains }),
+        new InjectedConnector({ chains, options: { shimDisconnect: true } }),
+        new CoinbaseWalletConnector({ chains, options: { appName: metadata.name } })
+      ],
+      publicClient
+ })
 
 createWeb3Modal({ wagmiConfig, projectId, chains})
 
